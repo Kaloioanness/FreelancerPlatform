31e8c227efc115adbc1ffb88b09806de6d9f12da
Kaloyan Dimitrov <Kaloyandimitrov97@abv.bg>
2024-07-27 17:37:43 +0300

    i18n

--- src/main/java/softuni/bg/controller/HomeController.java
+++ src/main/java/softuni/bg/controller/HomeController.java
@@ -1,17 +1,25 @@
 package softuni.bg.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import softuni.bg.model.FreelanceUserDetails;
 
 @Controller
 public class HomeController {
+
     @GetMapping("/")
-    public String home() {
+    public String home(@AuthenticationPrincipal UserDetails userDetails,
+                       Model model) {
+
+        if (userDetails instanceof FreelanceUserDetails freelanceUserDetails) {
+            model.addAttribute("welcomeMessage", freelanceUserDetails.getFullName());
+        } else {
+            model.addAttribute("welcomeMessage", "Anonymous");
+        }
+
         return "home";
     }
-    @GetMapping("/users/login")
-    public String login() {
-        return "login";
-    }
-
 }
