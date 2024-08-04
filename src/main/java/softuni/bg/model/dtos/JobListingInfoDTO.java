e098b1bf87046a20d1b39dad3413fcf41fee1453
Kaloyan Dimitrov <Kaloyandimitrov97@abv.bg>
2024-08-04 19:43:24 +0300

    adding new logic and htmls

---
+++ src/main/java/softuni/bg/model/dtos/JobListingInfoDTO.java
@@ -0,0 +1,83 @@
+package softuni.bg.model.dtos;
+
+import softuni.bg.model.entity.UserEntity;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class JobListingInfoDTO {
+    private Long id;
+    private String title;
+    private String description;
+    private BigDecimal budget;
+    private String skillsRequired;
+    private UserEntity client;
+    private LocalDateTime datePosted;
+
+    private int applicationsCount;
+    //getters and setters
+
+    public int getApplicationsCount() {
+        return applicationsCount;
+    }
+
+    public void setApplicationsCount(int applicationsCount) {
+        this.applicationsCount = applicationsCount;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getBudget() {
+        return budget;
+    }
+
+    public void setBudget(BigDecimal budget) {
+        this.budget = budget;
+    }
+
+    public String getSkillsRequired() {
+        return skillsRequired;
+    }
+
+    public void setSkillsRequired(String skillsRequired) {
+        this.skillsRequired = skillsRequired;
+    }
+
+    public UserEntity getClient() {
+        return client;
+    }
+
+    public void setClient(UserEntity client) {
+        this.client = client;
+    }
+
+    public LocalDateTime getDatePosted() {
+        return datePosted;
+    }
+
+    public void setDatePosted(LocalDateTime datePosted) {
+        this.datePosted = datePosted;
+    }
+}
