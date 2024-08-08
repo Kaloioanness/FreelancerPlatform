c033902c06a85aaaf07296166d7334627f56652d
Kaloyan Dimitrov <Kaloyandimitrov97@abv.bg>
2024-08-08 23:25:31 +0300

    modify UserController.java

--- src/main/java/softuni/bg/controller/UserController.java
+++ src/main/java/softuni/bg/controller/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import softuni.bg.model.dtos.UserDTO;
 import softuni.bg.model.dtos.UserRegistrationDTO;
 import softuni.bg.model.enums.RoleName;
@@ -34,12 +35,14 @@ public class UserController {
     }
 
     @PostMapping("/register")
-    public String register(@Valid @ModelAttribute("registerDTO") UserRegistrationDTO registerDTO, BindingResult result, Model model) {
-        if (result.hasErrors()) {
-            return "register"; //
+    public String register(@Valid @ModelAttribute("registerDTO") UserRegistrationDTO registerDTO, BindingResult bindingResult, RedirectAttributes rda, Model model) {
+        if (bindingResult.hasErrors() || !registerDTO.getPassword().equals(registerDTO.getConfirmPassword())) {
+            rda.addFlashAttribute("registerDTO", registerDTO);
+            rda.addFlashAttribute("org.springframework.validation.BindingResult.registerDTO", bindingResult);
+            return "/register"; //
         }
-        RoleName roleName = RoleName.valueOf(registerDTO.getRole().toString());
 
+        RoleName roleName = RoleName.valueOf(registerDTO.getRole().toString());
         userService.registerUser(registerDTO, roleName);
         return "redirect:/login";
     }
