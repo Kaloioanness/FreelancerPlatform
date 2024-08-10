0dc7a6fedd5816d4a83d0b03a98b0f4ad5a96083
Kaloyan Dimitrov <Kaloyandimitrov97@abv.bg>
2024-08-10 08:16:49 +0300

    adding scheduling

--- src/main/java/softuni/bg/FreelancerPlatform2Application.java
+++ src/main/java/softuni/bg/FreelancerPlatform2Application.java
@@ -2,8 +2,10 @@ package softuni.bg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class FreelancerPlatform2Application {
 
 	public static void main(String[] args) {
